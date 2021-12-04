295c1605dcd4fb914ff9add28e27aa77e02e7568
Aego <aego.yu@gmail.com>
2021-12-05 00:17:43 +0800

    fixed middleware type

--- src/http/core/middleware.ts
+++ src/http/core/middleware.ts
@@ -1,38 +1,24 @@
 import {
-  onRequestHookHandler,
-  onRequestAsyncHookHandler,
   RawServerDefault,
   RawRequestDefaultExpression,
   RawReplyDefaultExpression,
+  RouteHandler,
 } from 'fastify'
 import { Static } from '../../common/schema'
 import { Maybe, Schema } from './router'
 
-export type Middleware<S extends Schema = any> =
-  | onRequestHookHandler<
-      RawServerDefault,
-      RawRequestDefaultExpression<RawServerDefault>,
-      RawReplyDefaultExpression<RawServerDefault>,
-      {
-        Params: Maybe<Static<S['params']>, unknown>
-        Querystring: Maybe<Static<S['querystring']>, unknown>
-        Body: Maybe<Static<S['body']>, unknown>
-        Headers: Maybe<Static<S['headers']>, unknown>
-        Reply: Maybe<Static<S['response']>, unknown>
-      }
-    >
-  | onRequestAsyncHookHandler<
-      RawServerDefault,
-      RawRequestDefaultExpression<RawServerDefault>,
-      RawReplyDefaultExpression<RawServerDefault>,
-      {
-        Params: Maybe<Static<S['params']>, unknown>
-        Querystring: Maybe<Static<S['querystring']>, unknown>
-        Body: Maybe<Static<S['body']>, unknown>
-        Headers: Maybe<Static<S['headers']>, unknown>
-        Reply: Maybe<Static<S['response']>, unknown>
-      }
-    >
+export type Middleware<S extends Schema = any> = RouteHandler<
+  {
+    Params: Maybe<Static<S['params']>, unknown>
+    Querystring: Maybe<Static<S['querystring']>, unknown>
+    Body: Maybe<Static<S['body']>, unknown>
+    Headers: Maybe<Static<S['headers']>, unknown>
+    Reply: Maybe<Static<S['response']>, unknown>
+  },
+  RawServerDefault,
+  RawRequestDefaultExpression<RawServerDefault>,
+  RawReplyDefaultExpression<RawServerDefault>
+>
 
 export const useMiddleware = <S extends Schema>(middleware: Middleware<S>) =>
   middleware
