d22857051ed47c152fddb6e8ffbdc4b270915e4b
Aego <aego.yu@gmail.com>
2021-12-09 21:25:06 +0800

    added InstanceManager and useInstance

---
+++ src/utils/instance-manager.ts
@@ -0,0 +1,29 @@
+export class InstanceManager {
+  private static instances: Record<string, any> = {}
+
+  static put<T>(name: string, instance: T) {
+    this.instances[name] = instance
+  }
+
+  static get<T>(name: string) {
+    if (!this.instances[name]) {
+      throw new Error(`instance [${name}] is not found`)
+    }
+
+    return this.instances[name] as T
+  }
+}
+
+export type InstanceRef<T> = { instance: T }
+
+export const useInstance = <T>(name: string, instance?: T): InstanceRef<T> => {
+  if (instance) {
+    InstanceManager.put(name, instance)
+  }
+
+  return {
+    get instance() {
+      return InstanceManager.get<T>(name)
+    },
+  }
+}
