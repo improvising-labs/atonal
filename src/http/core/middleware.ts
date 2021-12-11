f138dd0b0cc8c7a2995cda0c8d5f69c63e59d007
Aego <aego.yu@gmail.com>
2021-12-11 14:54:34 +0800

    fixed middleware type bug

--- src/http/core/middleware.ts
+++ src/http/core/middleware.ts
@@ -1,12 +1,8 @@
 import { RouteHandler } from 'fastify'
 import { RouteGenericInterface } from 'fastify/types/route'
 
-export type Middleware<
-  T extends RouteGenericInterface = RouteGenericInterface,
-> = RouteHandler<T>
+export type Middleware<T extends RouteGenericInterface = any> = RouteHandler<T>
 
-export const useMiddleware = <
-  T extends RouteGenericInterface = RouteGenericInterface,
->(
+export const useMiddleware = <T extends RouteGenericInterface = any>(
   middleware: Middleware<T>,
 ) => middleware
