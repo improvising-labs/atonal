d6ac76603056fe5ac95c4960c42bd582040d3d27
Aego <aego.yu@gmail.com>
2021-12-27 06:18:54 +0800

    force middleware to be async

--- src/http/core/middleware.ts
+++ src/http/core/middleware.ts
@@ -1,9 +1,18 @@
-import { RouteHandler } from 'fastify'
+import {
+  preHandlerAsyncHookHandler,
+  RawReplyDefaultExpression,
+  RawRequestDefaultExpression,
+  RawServerDefault,
+} from 'fastify'
 import { RouteSchema, RouteSchemaInterface } from './schema'
 
-export type Middleware<S extends RouteSchema = RouteSchema> = RouteHandler<
-  RouteSchemaInterface<S>
->
+export type Middleware<S extends RouteSchema = RouteSchema> =
+  preHandlerAsyncHookHandler<
+    RawServerDefault,
+    RawRequestDefaultExpression<RawServerDefault>,
+    RawReplyDefaultExpression<RawServerDefault>,
+    RouteSchemaInterface<S>
+  >
 
 export const useMiddleware = <S extends RouteSchema = RouteSchema>(
   middleware: Middleware<S>,
