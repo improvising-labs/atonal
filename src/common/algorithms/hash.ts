ee997605cfce1f92f3aa8732c77d7f85988e58d6
Aego <aego.yu@gmail.com>
2022-01-04 18:04:21 +0800

    added more hash algorithms

--- src/common/algorithms/hash.ts
+++ src/common/algorithms/hash.ts
@@ -10,13 +10,19 @@ export const sha256 = (
   encoding: BinaryToTextEncoding = 'hex',
 ) => createHash('sha256').update(str).digest(encoding)
 
+export const sha512 = (
+  str: string | Buffer,
+  encoding: BinaryToTextEncoding = 'hex',
+) => createHash('sha512').update(str).digest(encoding)
+
+export const md4 = (
+  str: string | Buffer,
+  encoding: BinaryToTextEncoding = 'hex',
+) => createHash('md4').update(str).digest(encoding)
+
 export const md5 = (
   str: string | Buffer,
   encoding: BinaryToTextEncoding = 'hex',
 ) => createHash('md5').update(str).digest(encoding)
 
-export const hashPassword = (password: string, salt = '') =>
-  sha256(password + salt, 'base64')
-
-export const comparePassword = (salt: string, hash: string, password: string) =>
-  hashPassword(password + salt) === hash
+export const hashPassword = (password: string) => sha256(password, 'base64')
